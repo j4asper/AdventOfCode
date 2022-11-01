7caa4cd5bfcc3b4491d426e861a8831c5a317cd3
Jasper <jasper@jazper.dk>
2022-11-01 16:58:11 +0100

    Add december 3rd solution

---
+++ 2021/3/Part 1.py
@@ -0,0 +1,41 @@
+
+
+def get_most_common_bit(binary_list:list, index:int):
+    # Rather than making one_count and zero_count variables, we can check if the amount of zeros is larger og less than half the amount of entries in binary_list to decide which is the most common
+    half_list_entries = len(binary_list)/2
+    number_of_zeros = 0
+    for binary_string in binary_list:
+        if binary_string[index] == "0":
+            number_of_zeros += 1
+    
+    return 0 if number_of_zeros > half_list_entries else 1
+    
+def calculate_power_consumption(binary_list:list):
+    gamma_rate = []
+    epsilon_rate = []
+    
+    binary_string_length = len(binary_list[0])
+    for index in range(0, binary_string_length-1):
+        bit = get_most_common_bit(binary_list, index)
+        if bit == 0:
+            gamma_rate.append("0")
+            epsilon_rate.append("1")
+        else:
+            gamma_rate.append("1")
+            epsilon_rate.append("0")
+    
+    
+    gamma_rate_int = int("".join(gamma_rate), 2)
+    epsilon_rate_int = int("".join(epsilon_rate), 2)
+    return gamma_rate_int * epsilon_rate_int
+    
+    
+    
+
+if __name__ == "__main__":
+    # First convert the input data to a list of binary numbers.
+    with open("2021\\3\\input.txt", "r") as f:
+        binary_list = f.readlines()
+    
+    print("What is the power consumption of the submarine?")    
+    print("Puzzle answer: ", calculate_power_consumption(binary_list))
