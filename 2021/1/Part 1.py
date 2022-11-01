046bf3762763e1cbfec82457178dfc4d51bfeb50
Jasper <jasper@jazper.dk>
2022-11-01 13:14:21 +0100

    Add december 1st solution

---
+++ 2021/1/Part 1.py
@@ -0,0 +1,28 @@
+def count_measurement_increases(measurements:list):
+    """Counts the number of measurement increases in the given list of depths
+
+    Args:
+        input (list): List of depth measurements
+    """
+    previous_measurement = None
+    increases = 0
+    for measurement in measurements:
+        if previous_measurement != None:
+            if measurement > previous_measurement:
+                increases += 1
+                
+        previous_measurement = measurement
+    
+    return increases
+
+
+
+
+
+if __name__ == "__main__":
+    # First convert the input data to a list of measurements.
+    with open("2021\\1\\input.txt", "r") as f:
+        measurements = [int(measurement) for measurement in f.readlines()]
+    
+    print("How many measurements are larger than the previous measurement?")
+    print("Puzzle answer: ", count_measurement_increases(measurements))
