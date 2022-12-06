340c47121a7cb9169d6d13832212deff93bd1ba1
Jasper <jasper@jazper.dk>
2022-12-06 21:49:17 +0100

    Solve december 6th

---
+++ 2022/6/Part 1.py
@@ -0,0 +1,29 @@
+def duplicate_in_list(four_chars:list):
+    for char in four_chars:
+        occurrences = four_chars.count(char)
+        if occurrences != 1:
+            return True
+    return False
+
+def find_marker(datastream_buffer:str):
+    start_of_packet_marker = 1
+    four_chars = list()
+    for char in datastream_buffer:
+        if start_of_packet_marker > 4:
+            four_chars.pop(0)
+        four_chars.append(char)
+        if len(four_chars) == 4:
+            duplicate = duplicate_in_list(four_chars)
+            if not duplicate:
+                return start_of_packet_marker
+        start_of_packet_marker += 1
+            
+
+if __name__ == "__main__":
+    # Get datastream buffer from input file
+    from pathlib import Path
+    with open(Path(__file__).parent.resolve() / "input.txt", "r") as f:
+        datastream_buffer = f.readline()
+    
+    print("How many characters need to be processed before the first start-of-packet marker is detected?")
+    print("Puzzle answer: ", find_marker(datastream_buffer))
